1e7d356bf7af5ed434d1eba2e7d7c3b0c3a05edb
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-09-10 16:24:51 +0300

    Initial Commit

---
+++ HW 3.5 Gnilitskiy/HW_3_5_GnilitskiyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_5_GnilitskiyApp.swift
+//  HW 3.5 Gnilitskiy
+//
+//  Created by Александр on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_5_GnilitskiyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
